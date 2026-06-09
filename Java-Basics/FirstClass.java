ebf5bbdf2c640c44fa6e9a8ae9d97ec1e2499afb
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-09 21:53:59 +0530

    Day 1: Added FirstClass and Main programs

---
+++ Java-Basics/FirstClass.java
@@ -0,0 +1,14 @@
+class Firstclass{ 
+    public static void main(String args[]){
+         System.out.print("*\n");
+         System.out.println("**");
+         System.out.println("***");
+         System.out.println("****");
+         System.out.println("*****");
+         String name= "Gurjar";
+         int a=10;
+         System.out.println(name+" "+a);
+         
+    }
+}
+ 
