316ae2c550a3c174b4ed22ac2508bff0f31f8ebb
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-16 22:52:50 +0530

    Variables in java

---
+++ Java-Basics/Variables.java
@@ -0,0 +1,15 @@
+package DSA_Java.JavaBasics ;
+// Demonstrates how to declare and use variables in Java.
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Anjali";
+        double cgpa = 9.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
