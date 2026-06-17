4f22acdbbc3bdca908f758e2a4e863676ff6c3b1
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-17 20:44:08 +0530

    DAtatype in java

---
+++ Java-Basics/DataTypes.java
@@ -0,0 +1,29 @@
+// DataTypes in java
+// This program demonstrates the use of primitive data types in Java.
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte age = 20;
+        short year = 2026;
+        int population = 1000000;
+        long mobileNumber = 9876543210L;
+
+        float percentage = 85.5f;
+        double pi = 3.14159265359;
+
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Byte Value: " + age);
+        System.out.println("Short Value: " + year);
+        System.out.println("Int Value: " + population);
+        System.out.println("Long Value: " + mobileNumber);
+
+        System.out.println("Float Value: " + percentage);
+        System.out.println("Double Value: " + pi);
+
+        System.out.println("Character Value: " + grade);
+        System.out.println("Boolean Value: " + isJavaFun);
+    }
+}
