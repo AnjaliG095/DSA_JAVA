d53d8a2048f7d5b7d396d22e0f779427072ed708
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-22 00:37:35 +0530

    Shortest Path

---
+++ Strings/ShortestPath.java
@@ -0,0 +1,53 @@
+// Program: Find the shortest path from a given route.
+//
+// Logic:
+// 1. Start from origin (0,0).
+// 2. Update x and y coordinates based on direction.
+// 3. Find the final position.
+// 4. Use distance formula to calculate shortest path.
+//
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+package Strings;
+
+public class ShortestPath {
+
+    public static float getShortestPath(String path) {
+
+        int x = 0, y = 0;
+
+        for(int i = 0; i < path.length(); i++) {
+
+            char dir = path.charAt(i);
+
+            // South
+            if(dir == 'S') {
+                y--;
+            }
+            // North
+            else if(dir == 'N') {
+                y++;
+            }
+            // West
+            else if(dir == 'W') {
+                x--;
+            }
+            // East
+            else {
+                x++;
+            }
+        }
+
+        int X2 = x * x;
+        int Y2 = y * y;
+
+        return (float)Math.sqrt(X2 + Y2);
+    }
+
+    public static void main(String[] args) {
+
+        String path = "WNEENESENNN";
+
+        System.out.println(getShortestPath(path));
+    }
+}
