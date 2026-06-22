28ea1909f3de425b68be8f6d07973ac284825c96
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-22 17:34:48 +0530

    Checking OddEven in Binary digit

---
+++ BitManupulation/OddEven.java
@@ -0,0 +1,18 @@
+package BitManupulation;
+
+public class OddEven
+{
+    public static void checkOddEven(int n){
+        // Important concept
+        if((n&1) ==0){
+            System.out.println(n+ "is even");
+
+        }else{
+            System.out.println(n + "is Odd");
+        }
+    }
+    public static void main (String[] args){
+        checkOddEven(13);
+        checkOddEven(45);
+    }
+}
