ff8ebdbcd7d0f2bee04daa0f72a9594b2a3b314f
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-23 17:06:11 +0530

    Updating Bit in Binary Number

---
+++ BitManupulation/UpdateBit.java
@@ -0,0 +1,16 @@
+package BitManupulation;
+
+public class UpdateBit {
+    public static int updateBit(int num , int position ,int newBit){
+        num = num & (~(1 << position));
+        int bitMask = newBit << position;
+        return num | bitMask ;
+    }
+public static void main(String[] args) {
+    System.out.println(updateBit(10,1,1));
+    System.out.println(updateBit(10,3,0));
+}
+    
+}
+// It was 1010 then it Update to 1010 only 
+// 2nd case 1010 it was after update its 3 value will update to 0 os it will become 0010 = 2.
