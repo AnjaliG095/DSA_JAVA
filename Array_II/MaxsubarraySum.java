1faa5335e7dc89e069766015e03fa31a2e96aa40
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-25 20:28:56 +0530

    Subarray Sum (Max)

---
+++ Array_II/MaxsubarraySum.java
@@ -0,0 +1,37 @@
+// Program: Find the Maximum Subarray Sum using the Brute Force approach.
+// Output Example: Maximum Sum = 37
+//
+// Time Complexity: O(n³)
+// Space Complexity: O(1)
+
+public class MaxsubarraySum{ 
+   public static void  maxSubarraySum(int arr[]) {
+   
+           int maxsum = Integer.MIN_VALUE; //-infintiy
+          for(int i =0;i<arr.length;i++){
+            int start=i;
+           
+          for(int j=i; j<arr.length; j++){ 
+                int end =j;
+                int sum = 0;
+                for(int k=start;k<=end;k++){
+                sum=sum+arr[k];
+                   
+            }
+            
+            System.out.println("Sum="+sum);
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+          }
+        
+   }
+        System.out.println("Maximum Sum = " + maxsum);
+   }
+   public static void main(String[] args) {
+       int arr[]= {2,4,6,8,8,9};
+       maxSubarraySum(arr);
+   }
+}
+    
+
