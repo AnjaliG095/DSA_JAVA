74066e4a2e619551f20aa5d64349f5b55f9a5f54
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-30 00:03:02 +0530

    Constructors

---
+++ OOPs/Constructors.java
@@ -0,0 +1,48 @@
+package OOPS;
+
+/*
+ *
+ * Constructor:
+ * A constructor is a special method that is automatically called
+ * when an object is created.
+*/
+
+class Student {
+
+    String name;
+    int age;
+
+   
+    // CONSTRUCTOR DEFINITION
+    
+    Student(String name, int age) {
+
+        // 'this' refers to the current object
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Constructors {
+
+    public static void main(String[] args) {
+
+       
+        // CONSTRUCTOR CALL / USAGE
+        Student s1 = new Student("Anjali", 20);
+
+        // Displaying object data
+        s1.display();
+    }
+}
+
+/*
+Output:
+Name: Anjali
+Age: 20
+*/
