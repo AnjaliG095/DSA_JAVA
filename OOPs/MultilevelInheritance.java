3d21fc5cc63875899ff6150d97237d7e692f27c9
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-07-16 20:01:40 +0530

    Multi LEvel Inheritance

--- OOPs/MultilevelInheritance.java
+++ OOPs/MultilevelInheritance.java
@@ -1,5 +1,32 @@
-package OOPS;
+package DSA_Java.OOPs;
 
 public class MultilevelInheritance {
+    class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+class Puppy extends Dog {
+    void weep() {
+        System.out.println("Puppy is weeping");
+    }
+}
+
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+
+        p.eat();   // from Animal
+        p.bark();  // from Dog
+        p.weep();  // from Puppy
+    }
+}
     
 }
